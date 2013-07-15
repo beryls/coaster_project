4b2eb8d906c1f057411af849123c805bd11e0146
Beryl <beryl.schragger@gmail.com>
2013-07-15 15:20:38 -0400

    Create new rails application.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CoasterProject::Application.config.secret_token = '6987400618889a917e184f3c36b9c3e5e8617ab3c4c0d4c8500a524de683ec4c93b36663071491db8393d9ce68a876c1f4223a944d12f128bfc87e57a147da25'
