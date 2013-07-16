c318180f1f383c8b2ec3b897adc3a82cf0590617
Beryl <beryl.schragger@gmail.com>
2013-07-15 21:43:48 -0400

    Add attr_accessible to user model.

--- app/models/User.rb
+++ app/models/User.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   has_and_belongs_to_many :coasters
+  attr_accessible :name, :user_name, :email, :password
   validates :name, :user_name, :email, :password, presence: true
   validates :name, :user_name, :email, :password, length: { maximum: 40 }
   validates :user_name, :email, uniqueness: true
