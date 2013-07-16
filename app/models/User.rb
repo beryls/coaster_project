db99406faaba8ff7405ea6f2cfb707eca4a43c45
Beryl <beryl.schragger@gmail.com>
2013-07-15 21:41:41 -0400

    Add development gems and user validations.

--- app/models/User.rb
+++ app/models/User.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
   has_and_belongs_to_many :coasters
+  validates :name, :user_name, :email, :password, presence: true
+  validates :name, :user_name, :email, :password, length: { maximum: 40 }
+  validates :user_name, :email, uniqueness: true
+  validates :password, length: { minimum: 8 }
 end
