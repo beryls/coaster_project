f1f1b214afd77bb3cc8ec29fd01e2b9b567c3d93
Beryl <beryl.schragger@gmail.com>
2013-07-16 10:05:26 -0400

    Add ability to create new user, basic show user page, and placeholder code for index page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,23 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new
+    @user.name = params[:name]
+    @user.user_name = params[:user_name]
+    @user.email = params[:email]
+    @user.password = params[:password]
+    @user.save
+    redirect_to '/users'
   end
 end
