f81e383bee271d94635a77dc13713ea2fa01ed2d
Beryl <beryl.schragger@gmail.com>
2013-07-16 21:37:11 -0400

    Make coasters index page the search page for coasters.

--- app/controllers/coasters_controller.rb
+++ app/controllers/coasters_controller.rb
@@ -1,9 +1,13 @@
 class CoastersController < ApplicationController
   def index
-    @parks = Park.all
+    @coasters = Coaster.where(name: params[:query])
   end
 
   def show
     @coaster = Coaster.find(params[:id])
+  end
+
+  def search
+    @coasters = Coaster.where(name: params[:query])
   end
 end
