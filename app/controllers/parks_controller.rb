c675e5ac5028c71475ced8f109131c9aca235f13
Beryl <beryl.schragger@gmail.com>
2013-07-16 20:07:31 -0400

    Add page to view all parks and individual park pages.

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -1,4 +1,10 @@
 class ParksController < ApplicationController
   def index
+    @parks = Park.all
+  end
+
+  def show
+    @park = Park.find(params[:id])
+    @coasters = Coaster.where(park_id: @park.id)
   end
 end
